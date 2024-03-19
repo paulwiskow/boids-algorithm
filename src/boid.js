7703ba3f538ac55056274a3200765359d1893049
Paul Wiskow <usk7dg@virginia.edu>
2024-03-19 15:39:50 -0400

    Redid how the boid's velocity and orientation were calculated and added some functions for updating the board.

--- src/boid.js
+++ src/boid.js
@@ -3,14 +3,20 @@ const DEFAULT_WIDTH = 500;
 const DEFAULT_HEIGHT = 500;
 const MIN_VELOCITY = 1;
 const MAX_VELOCITY = 5;
-const DEFAULT_CLEARANCE = 20;  // If the boids cross this default_clearance, then we need to reroute them back
+const DEFAULT_CLEARANCE = 20;  
 
 class Boid {
     constructor() {
         this.x = Math.floor((Math.random() * (DEFAULT_WIDTH - DEFAULT_CLEARANCE * 2)) + DEFAULT_CLEARANCE);
         this.y = Math.floor((Math.random() * (DEFAULT_HEIGHT - DEFAULT_CLEARANCE * 2)) + DEFAULT_CLEARANCE);
 
-        this.vx = Math.floor((Math.random() * MAX_VELOCITY) + MIN_VELOCITY);
-        this.vy = Math.floor((Math.random() * MAX_VELOCITY) + MIN_VELOCITY);
+        // this.v = Math.floor((Math.random() * MAX_VELOCITY) + MIN_VELOCITY);
+        this.v = 3;  // Have a default speed for all boids now
+        this.orientation = Math.floor(Math.random() * 360);  // 0 degrees on positive x axis
+    }
+
+    update_position() {
+        this.x += Math.floor(this.v * Math.cos(this.orientation * (Math.PI / 180)));  // Have to convert to radians
+        this.y += Math.floor(this.v * Math.sin(this.orientation * (Math.PI / 180)));
     }
 }
--- src/canvas.js
+++ src/canvas.js
@@ -1,7 +1,8 @@
 // handles the canvas and all the information in it -> will be the one to actually draw the boids
 const DEFAULT_WIDTH = 500;
 const DEFAULT_HEIGHT = 500;
-const DEFAULT_CLEARANCE = 20;
+const DEFAULT_CLEARANCE = 20;  // If the boids cross this default_clearance, then we need to reroute them back
+const FPS = 50;
 
 class Canvas {
     constructor() {
@@ -14,11 +15,12 @@ class Canvas {
     }
 
     start() {
-
+        this.interval(this.update, Math.floor(1000 / FPS))
     }
 
     update() {
-
+        this.clear();
+        // go through boid_list and update each one, will have to do some geometry with proximity to other boids as well as well as actually drawing the boids
     }
 
     clear() {
